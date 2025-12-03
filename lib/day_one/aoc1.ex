e8e599f6df9c72ae299eeaf0540def2caed66293
Nikita Karpukhin <gray@graynk.space>
2025-12-03 13:59:43 +0100

    light housekeeping

--- lib/day_one/aoc1.ex
+++ lib/day_one/aoc1.ex
@@ -1,12 +1,12 @@
 defmodule Aoc.DayOne do
-  def parse(<<"R", num::binary>>), do: String.to_integer(num)
-  def parse(<<"L", num::binary>>), do: -1 * String.to_integer(num)
+  defp parse(<<"R", num::binary>>), do: String.to_integer(num)
+  defp parse(<<"L", num::binary>>), do: -1 * String.to_integer(num)
 
-  def rotate(delta, dial) do
+  defp rotate(delta, dial) do
     Integer.mod(delta + dial, 100)
   end
 
-  def get_clicks(delta, dial) do
+  defp get_clicks(delta, dial) do
     sum = dial + rem(delta, 100)
     rotations = div(abs(delta), 100)
 
@@ -16,12 +16,12 @@ defmodule Aoc.DayOne do
     end
   end
 
-  def reduce_part_one(line, {acc, count}) do
+  defp reduce_part_one(line, {acc, count}) do
     result =
       line
       |> String.trim()
-      |> Aoc.DayOne.parse()
-      |> Aoc.DayOne.rotate(acc)
+      |> parse()
+      |> rotate(acc)
 
     case result do
       0 -> {0, count + 1}
@@ -29,19 +29,21 @@ defmodule Aoc.DayOne do
     end
   end
 
-  def reduce_part_two(line, {dial, count}) do
+  defp reduce_part_two(line, {dial, count}) do
     delta =
       line
       |> String.trim()
-      |> Aoc.DayOne.parse()
+      |> parse()
 
-    {Aoc.DayOne.rotate(delta, dial), count + Aoc.DayOne.get_clicks(delta, dial)}
+    {rotate(delta, dial), count + get_clicks(delta, dial)}
   end
 
   def part_one(path) do
     {_, result} =
       File.stream!(path)
-      |> Enum.reduce({50, 0}, &Aoc.DayOne.reduce_part_one/2)
+      |> Enum.map(fn line -> String.trim(line) end)
+      |> Enum.filter(fn line -> line != "" end)
+      |> Enum.reduce({50, 0}, &reduce_part_one/2)
 
     result
   end
@@ -49,7 +51,9 @@ defmodule Aoc.DayOne do
   def part_two(path) do
     {_, result} =
       File.stream!(path)
-      |> Enum.reduce({50, 0}, &Aoc.DayOne.reduce_part_two/2)
+      |> Enum.map(fn line -> String.trim(line) end)
+      |> Enum.filter(fn line -> line != "" end)
+      |> Enum.reduce({50, 0}, &reduce_part_two/2)
 
     result
   end
--- lib/day_two/aoc2.ex
+++ lib/day_two/aoc2.ex
@@ -1,12 +1,12 @@
 defmodule Aoc.DayTwo do
-  def parse(range) when is_binary(range) do
+  defp parse(range) when is_binary(range) do
     [start_str, finish_str] = String.split(range, "-")
     {String.to_integer(start_str), String.to_integer(finish_str)}
   end
 
-  def parse(_), do: {:error, :invalid_format}
+  defp parse(_), do: {:error, :invalid_format}
 
-  def repeat2?(bin) when is_binary(bin) do
+  defp repeat2?(bin) when is_binary(bin) do
     len = byte_size(bin)
 
     case rem(len, 2) do
@@ -22,26 +22,26 @@ defmodule Aoc.DayTwo do
     MatchError -> false
   end
 
-  def pattern_checker(bin, counter, len) do
+  defp pattern_checker(bin, counter, len) do
     symbols = counter + 1
 
     rem(len, symbols) == 0 and
       String.count(bin, String.slice(bin, 0..counter)) == div(len, symbols)
   end
 
-  def repeat?(bin, counter, len) when is_binary(bin) do
+  defp repeat?(bin, counter, len) when is_binary(bin) do
     cond do
       counter == -1 -> false
-      Aoc.DayTwo.pattern_checker(bin, counter, len) -> true
-      true -> Aoc.DayTwo.repeat?(bin, counter - 1, len)
+      pattern_checker(bin, counter, len) -> true
+      true -> repeat?(bin, counter - 1, len)
     end
   end
 
-  def reduce_part_one(range, count) do
-    {start, finish} = Aoc.DayTwo.parse(range)
+  defp reduce_part_one(range, count) do
+    {start, finish} = parse(range)
 
     Enum.reduce(start..finish, count, fn id, acc ->
-      case Aoc.DayTwo.repeat2?(Integer.to_string(id)) do
+      case repeat2?(Integer.to_string(id)) do
         true -> acc + id
         false -> acc
       end
@@ -49,14 +49,14 @@ defmodule Aoc.DayTwo do
   end
 
   # Eh. Just brute force it
-  def reduce_part_two(range, count) do
-    {start, finish} = Aoc.DayTwo.parse(range)
+  defp reduce_part_two(range, count) do
+    {start, finish} = parse(range)
 
     Enum.reduce(start..finish, count, fn id, acc ->
       id_str = Integer.to_string(id)
       len = byte_size(id_str)
 
-      case Aoc.DayTwo.repeat?(id_str, div(len, 2) - 1, len) do
+      case repeat?(id_str, div(len, 2) - 1, len) do
         true -> acc + id
         false -> acc
       end
@@ -67,13 +67,13 @@ defmodule Aoc.DayTwo do
     File.read!(path)
     |> String.trim()
     |> String.split(",")
-    |> Enum.reduce(0, &Aoc.DayTwo.reduce_part_one/2)
+    |> Enum.reduce(0, &reduce_part_one/2)
   end
 
   def part_two(path) do
     File.read!(path)
     |> String.trim()
     |> String.split(",")
-    |> Enum.reduce(0, &Aoc.DayTwo.reduce_part_two/2)
+    |> Enum.reduce(0, &reduce_part_two/2)
   end
 end
