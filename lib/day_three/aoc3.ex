6b96469c741f0dfb02c1ed5d091517589afc22f2
Nikita Karpukhin <gray@graynk.space>
2025-12-03 15:26:07 +0100

    use byte slices

--- lib/day_three/aoc3.ex
+++ lib/day_three/aoc3.ex
@@ -37,26 +37,24 @@ defmodule Aoc.DayThree do
   end
 
   # Part two
-
+  #
   defp find_largest_char(<<char::utf8, rest::binary>>, {_, max_char}, current_index, n)
+       when char > max_char and byte_size(rest) >= 12 - n do
+    find_largest_char(rest, {current_index, char}, current_index + 1, n)
+  end
+
+  defp find_largest_char(<<char::utf8, _::binary>>, {_, max_char}, current_index, _)
        when char > max_char do
-    if String.length(rest) >= 12 - n do
-      find_largest_char(rest, {current_index, char}, current_index + 1, n)
-    else
-      {current_index, char}
-    end
+    {current_index, char}
   end
 
-  defp find_largest_char(<<_::utf8, rest::binary>>, {max_index, max_char}, current_index, n) do
-    if String.length(rest) >= 12 - n do
-      find_largest_char(rest, {max_index, max_char}, current_index + 1, n)
-    else
-      {max_index, max_char}
-    end
+  defp find_largest_char(<<_::utf8, rest::binary>>, {max_index, max_char}, current_index, n)
+       when byte_size(rest) >= 12 - n do
+    find_largest_char(rest, {max_index, max_char}, current_index + 1, n)
   end
 
-  defp find_largest_char("", {max_index, max_char}, _, _) do
-    {max_index, max_char}
+  defp find_largest_char(_, result, _, _) do
+    result
   end
 
   defp search_twelve(_, chars) when length(chars) == 12 do
@@ -65,7 +63,7 @@ defmodule Aoc.DayThree do
 
   defp search_twelve(string, chars) do
     {index, char} = find_largest_char(string, {0, 0}, 0, length(chars))
-    search_twelve(String.slice(string, (index + 1)..String.length(string)), chars ++ [char])
+    search_twelve(binary_slice(string, (index + 1)..byte_size(string)), chars ++ [char])
   end
 
   defp reduce_part_two(line, sum) do
