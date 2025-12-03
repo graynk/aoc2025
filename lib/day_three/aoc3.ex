4415d0133164aaf6adb341471cee0e94827f20e2
Nikita Karpukhin <gray@graynk.space>
2025-12-03 15:21:34 +0100

    day 3 part 2

--- lib/day_three/aoc3.ex
+++ lib/day_three/aoc3.ex
@@ -3,6 +3,13 @@ defmodule Aoc.DayThree do
     String.to_integer(<<a::utf8, b::utf8>>)
   end
 
+  defp sum_chars(list) do
+    str = Enum.map_join(list, fn char -> <<char::utf8>> end)
+    String.to_integer(str)
+  end
+
+  # Part one
+
   defp search(<<char::utf8, rest::binary>>, {0, 0}) do
     search(rest, {char, 0})
   end
@@ -29,7 +36,41 @@ defmodule Aoc.DayThree do
     sum + search(line, {0, 0})
   end
 
-  # Eh. Just brute force it
+  # Part two
+
+  defp find_largest_char(<<char::utf8, rest::binary>>, {_, max_char}, current_index, n)
+       when char > max_char do
+    if String.length(rest) >= 12 - n do
+      find_largest_char(rest, {current_index, char}, current_index + 1, n)
+    else
+      {current_index, char}
+    end
+  end
+
+  defp find_largest_char(<<_::utf8, rest::binary>>, {max_index, max_char}, current_index, n) do
+    if String.length(rest) >= 12 - n do
+      find_largest_char(rest, {max_index, max_char}, current_index + 1, n)
+    else
+      {max_index, max_char}
+    end
+  end
+
+  defp find_largest_char("", {max_index, max_char}, _, _) do
+    {max_index, max_char}
+  end
+
+  defp search_twelve(_, chars) when length(chars) == 12 do
+    sum_chars(chars)
+  end
+
+  defp search_twelve(string, chars) do
+    {index, char} = find_largest_char(string, {0, 0}, 0, length(chars))
+    search_twelve(String.slice(string, (index + 1)..String.length(string)), chars ++ [char])
+  end
+
+  defp reduce_part_two(line, sum) do
+    sum + search_twelve(line, [])
+  end
 
   def part_one(path) do
     File.stream!(path)
@@ -38,10 +79,10 @@ defmodule Aoc.DayThree do
     |> Enum.reduce(0, &reduce_part_one/2)
   end
 
-  # def part_two(path) do
-  #   File.stream!(path)
-  #   |> Enum.map(fn line -> String.trim(line) end)
-  #   |> Enum.filter(fn line -> line != "" end)
-  #   |> Enum.reduce(0, &reduce_part_one/2)
-  # end
+  def part_two(path) do
+    File.stream!(path)
+    |> Enum.map(fn line -> String.trim(line) end)
+    |> Enum.filter(fn line -> line != "" end)
+    |> Enum.reduce(0, &reduce_part_two/2)
+  end
 end
--- test/day_three/aoc3_test.exs
+++ test/day_three/aoc3_test.exs
@@ -11,8 +11,13 @@ defmodule Aoc.DayThreeTest do
              17301
   end
 
-  # test "second part works on the test input" do
-  #   assert Aoc.DayThree.part_two("test/fixtures/day_three/testinput.txt") ==
-  #            3_121_910_778_619
-  # end
+  test "second part works on the test input" do
+    assert Aoc.DayThree.part_two("test/fixtures/day_three/testinput.txt") ==
+             3_121_910_778_619
+  end
+
+  test "second part works on the actual input" do
+    assert Aoc.DayThree.part_two("test/fixtures/day_three/input.txt") ==
+             172_162_399_742_349
+  end
 end
